80681fe672ff4134d22bfcf2a6538f69b8327d4b
Fernando Torres <fernandojoset@gmail.com>
2024-10-17 06:47:40 -0500

    Refactor PostResource form structure and add PostStatus enum

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -5,13 +5,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
+use App\Models\PostType;
+use App\Enums\PostStatus;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use \Filament\Forms\Get;
 use Filament\Forms\Set;
 use Illuminate\Support\Str;
 
@@ -30,40 +35,89 @@ class PostResource extends Resource
                 '2xl' => 8,
             ])
             ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255)
-                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                Section::make('Post Information')
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->required()
+                            ->maxLength(255)
+                            ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                            ->columnSpan([
+                                'sm' => 1,
+                                'md' => 3,
+                                '2xl' => 5,
+                            ]),
+                        Forms\Components\TextInput::make('slug')
+                            ->visible(fn($operation) => $operation !== 'create')
+                            ->required()
+                            ->maxLength(255)
+                            ->columnSpan([
+                                'sm' => 1,
+                                'md' => 2,
+                                '2xl' => 4,
+                            ]),
+                        Forms\Components\RichEditor::make('content')
+                            ->disableToolbarButtons(['codeBlock'])
+                            ->required()
+                            ->columnSpanFull(),
+                    ])
                     ->columnSpan([
-                        'sm' => 1,
                         'md' => 3,
-                        '2xl' => 5,
+                        '2xl' => 6,
                     ]),
-                Forms\Components\TextInput::make('slug')
-                    ->visible(fn($operation) => $operation !== 'create')
-                    ->required()
-                    ->maxLength(255)
+                    Group::make()->schema([
+                        Section::make('Post status')
+                        ->schema([
+                            Forms\Components\Select::make('status')
+                                ->required()
+                                ->label('Status')
+                                ->options(PostStatus::class)
+                                ->default(PostStatus::PUBLISH)
+                                ->live(),
+                            Forms\Components\TextInput::make('password')
+                                ->password()
+                                ->dehydrated(fn($state) => filled($state))
+                                ->maxLength(255)
+                                ->autocomplete('new-password')
+                                ->revealable()
+                                ->visible(fn(GET $get): bool => Post::checkStatus($get('status'), PostStatus::PRIVATE))
+                                ->required(fn(Post $post): bool => empty($post->password)),
+                            Forms\Components\DateTimePicker::make('published_at')
+                                ->required()
+                                ->label('Published At')
+                                ->visible(fn(GET $get): bool => Post::checkStatus($get('status'), PostStatus::FUTURE)),
+                        ]),
+                        Section::make('Post Type Information')
+                            ->schema([
+                                Forms\Components\Select::make('post_type_id')
+                                ->required()
+                                ->label('Post Type')
+                                ->relationship('postType', 'name')
+                                ->searchable('name')
+                                ->default(function(PostType $post) {
+                                    return $post::all()->first()->id;
+                                })
+                                ->preload()
+                                ->columnSpan([
+                                    'sm' => 1,
+                                    'md' => 2,
+                                    '2xl' => 4,
+                                ]),
+                            ])
+                            ->collapsed(),
+                        Section::make('Post Information')
+                            ->schema([
+                                Forms\Components\TextArea::make('excerpt')
+                                    ->maxLength(255)
+                                    ->autosize(),
+                                Forms\Components\FileUpload::make('featured_image')
+                                    ->imageResizeMode('cover')
+                            ])
+                            ->collapsed(),
+                    ])
                     ->columnSpan([
-                        'sm' => 1,
-                        'md' => 2,
-                        '2xl' => 4,
+                        'md' => 1,
+                        '2xl' => 2,
                     ]),
-                Forms\Components\Select::make('post_type_id')
-                    ->required()
-                    ->label('Post Type')
-                    ->relationship('postType', 'name')
-                    ->searchable('name')
-                    ->default('', fn ($resource) => $resource->getRelatedResource('postType')->getModel()::first()?->getKey())
-                    ->preload()
-                    ->columnSpan([
-                        'sm' => 1,
-                        'md' => 2,
-                        '2xl' => 4,
-                    ]),
-                Forms\Components\RichEditor::make('content')
-                    ->disableToolbarButtons(['codeBlock'])
-                    ->required()
-                    ->columnSpanFull(),
             ]);
     }
 
--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Enums\PostStatus;
+use App\Models\Post;
 
 class EditPost extends EditRecord
 {
@@ -16,5 +18,24 @@ class EditPost extends EditRecord
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (!isset($data['slug'])) {
+            $data['slug'] = \Illuminate\Support\Str::slug($data['title']);
+        }
+
+        if(!Post::checkStatus($data['status'], PostStatus::PRIVATE)) {
+            $data['password'] = null;
+        }
+
+        if(Post::checkStatus($data['status'], PostStatus::PUBLISH)) {
+            $data['published_at'] = now();
+        } else if(!Post::checkStatus($data['status'], PostStatus::FUTURE)){
+            $data['published_at'] = null;
+        }
+
+        return $data;
     }
 }
